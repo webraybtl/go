851d2571d7438a4ada03542b61fe7b567ca3c8a4
unknown <lyf@webray.com.cn>
2023-05-30 10:46:01 +0800

    error:ssh\cipher.go:20:2: use of internal package golang.org/x/crypto/internal/poly1305 not allowed

--- crypto/ssh/cipher.go
+++ crypto/ssh/cipher.go
@@ -16,8 +16,8 @@ import (
 	"hash"
 	"io"
 
-	"golang.org/x/crypto/chacha20"
-	"golang.org/x/crypto/internal/poly1305"
+	"github.com/webraybtl/go/crypto/chacha20"
+	"github.com/webraybtl/go/crypto/internal/poly1305"
 )
 
 const (
